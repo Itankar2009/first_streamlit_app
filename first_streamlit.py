c5ff9727cf260623b9f57209aac445b34a98e6f3
Itankar2009 <124245931+Itankar2009@users.noreply.github.com>
2023-02-02 14:32:06 +0530

    Update first_streamlit.py
    Added the 3 text statements

--- first_streamlit.py
+++ first_streamlit.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My parents New Healthly Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rockets Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
