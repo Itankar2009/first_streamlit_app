eec180fbee0ae6a029a909821aed8a5c6455840f
Itankar2009 <124245931+Itankar2009@users.noreply.github.com>
2023-02-02 14:40:43 +0530

    Update first_streamlit.py
    Added the emoji by just coping from snowflake learn platform & pasting to github pyhon file.
    Also added new header

--- first_streamlit.py
+++ first_streamlit.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My parents New Healthly Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rockets Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rockets Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
